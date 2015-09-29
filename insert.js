6043e364e5681693cccbe1c97dad63272de4aaa4
Tatyana Balabanova <balabanova@rutarget.ru>
2015-09-29 19:27:03 +0300

    can replace embed object

--- insert.js
+++ insert.js
@@ -1,6 +1,8 @@
 function checkSize( item, rtgBanner ) {
-  return item.clientWidth >= rtgBanner.width - 5 && item.clientWidth <= rtgBanner.width + 5 &&
-    item.clientHeight >= rtgBanner.height - 5 && item.clientHeight <= rtgBanner.height + 5;
+  return item.clientWidth >= rtgBanner.width - 10 && item.clientWidth <= rtgBanner.width + 10 &&
+    item.clientHeight >= rtgBanner.height - 10 && item.clientHeight <= rtgBanner.height + 10 ||
+    item.width >= rtgBanner.width - 10 && item.width <= rtgBanner.width + 10 &&
+    item.height >= rtgBanner.height - 10 && item.height <= rtgBanner.height + 10;
 }
 
 function findParent( item, rtgBanner ) {
@@ -35,12 +37,14 @@ function getGoodItems( items, goodItems, rtgBanner, getParent ) {
 }
 
 var iframes = document.getElementsByTagName( 'iframe' ),
-  images = document.getElementsByTagName( 'img' );
+  images = document.getElementsByTagName( 'img' ),
+  objects = document.getElementsByTagName( 'object' );
 
 var goodItems = [];
 
 getGoodItems( iframes, goodItems, rtgBanner, false );
 getGoodItems( images, goodItems, rtgBanner, true );
+getGoodItems( objects, goodItems, rtgBanner, true );
 
 if ( goodItems.length > 0 ) {
   if ( goodItems[0].isIframe ) {
